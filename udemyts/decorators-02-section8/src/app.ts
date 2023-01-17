f3e445112ea7a558c78948ab07bf459bff85cf47
Nathalie Surman <onepiecenat@hotmail.com>
2023-01-17 14:58:19 -0500

    decorator section 8 done

---
+++ udemyts/decorators-02-section8/src/app.ts
@@ -0,0 +1,231 @@
+
+//===== Decorators ===========//
+//--NOTE: if you want to use Decorators in ur projects make sure you go to the tsconfig.json file to turn theses on appropriately --
+//-- make sure this is  "target": "es6" then ---> ,"experimentalDecorators": true comment in  ---//
+// function Logger(constructor: Function) {
+//   console.log('Logging...');
+//   console.log(constructor);
+// }
+
+//---This is a Decorator Factories----//
+function Logger(logString: string) {
+  //--Now we have a function inside a function--//
+  return function(constructor: Function){
+    console.log(logString);
+    console.log(constructor);
+  }
+
+}
+
+
+// function WithTemplate(template: string, hookId: string){
+//   //----- NOTE: we put _: when we want to tell Ts that " yes we have a constructor but we don't want to use it yet ---"
+//   return function(_: Function) {
+//     const hookEl = document.getElementById(hookId)
+//     if (hookEl){
+//       hookEl.innerHTML = template
+//     }
+//   }
+// }
+
+
+
+function WithTemplate(template: string, hookId: string) {
+  console.log('TEMPLATE FACTORY');
+  return function<T extends { new (...args: any[]): {name: string} }>(
+    originalConstructor: T
+  ) {
+    //--this is  Returning and changing a class in a class Decorator---//
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log('Rendering template');
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector('h1')!.textContent = this.name;
+        }
+      }
+    };
+  };
+}
+
+//--This points at a function--//
+@Logger("TEST-LOGGING - PERSON")
+@WithTemplate("<h1>My Person Object</h1>", "app")
+
+class Person {
+  name = 'Robin';
+
+  constructor() {
+    console.log('Creating person object...');
+  }
+}
+
+const pers = new Person();
+
+console.log(pers);
+
+///======= Parameter Decorators ======///
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+  
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+  
+}
+
+function Log3(target: any, name:  string | Symbol, descriptor: PropertyDescriptor) {
+  console.log("Method decorator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+  
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter decorator");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+  
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error('Invalid price - it needs to be positive!');
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+
+}
+
+const p1 = new Product('Book', 19);
+const p2 = new Product('Book 2', 29);
+
+function Autobind(_:any, _2: string, descriptor: PropertyDescriptor){
+  const originalMethod = descriptor.value
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  };
+  return adjDescriptor;
+}
+class Printer {
+  message = "This works test"
+  
+  @Autobind
+  //--Method--
+  showMessage(){
+    console.log(this.message);
+    
+  }
+}
+
+const p = new Printer
+
+const button = document.querySelector("button")!
+button?.addEventListener("click", p.showMessage)
+
+//======== Decorators with Validation ====//
+interface ValidatorConfig {
+  [property: string]: {
+    [validatableProp: string]: string[]; // ['required', 'positive']
+  };
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['required']
+  };
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['positive']
+  };
+}
+
+
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) {
+    return true;
+  }
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case 'required':
+          isValid = isValid && !!obj[prop];
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course{
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number
+
+  constructor(t: string, p: number) {
+    this.title = t
+    this.price = p
+  }
+}
+
+const courseForm = document.querySelector("form")! // --> the ! at the end is to tell Ts that you know form is there in the HTML
+courseForm.addEventListener("submit", event =>{
+  event.preventDefault()
+  const titleEl = document.getElementById("title") as HTMLInputElement
+  const priceEl = document.getElementById("price") as HTMLInputElement
+
+  const title = titleEl.value
+  const price = +priceEl.value // --> the + in front of price is so tha we can convert it to a number
+
+  const createdCourse = new Course(title, price)
+  if (!validate(createdCourse)) {
+    alert('Invalid input, please try again!');
+    return;
+  }
+  console.log(createdCourse);
+  
+})
