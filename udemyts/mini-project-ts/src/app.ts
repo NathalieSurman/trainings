63170a038dfe224d32f9b97aa56ddaf07db0bf3e
Nathalie Surman <onepiecenat@hotmail.com>
2023-01-18 15:42:01 -0500

    adding validations and created new class

--- udemyts/mini-project-ts/src/app.ts
+++ udemyts/mini-project-ts/src/app.ts
@@ -1,5 +1,43 @@
 // Code goes here!
 
+//--Validation logic ---//
+//--NOTE: this also needs to be on top of the class and bind when using it ---//
+
+interface Validatable {
+    //-- These are the properties we want to support ---//
+    value: string | number
+    required?: boolean
+    minLength?: number
+    maxLength?: number
+    min?: number
+    max?: number
+}
+
+//-- THis validate(validatableInput: Validatable) gets an object which has the structure--//
+function validate(validatableInput: Validatable){
+    let isValid = true
+    //-- We want to check if it is empty--//
+    if (validatableInput.required){
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0
+    }
+    //--This checks if we do have a string when the minlength is set
+    if (validatableInput.minLength != null && typeof validatableInput.value === "string"){
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength
+    }
+
+    if (validatableInput.maxLength != null && typeof validatableInput.value === "string"){
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength
+    }
+
+    if (validatableInput.min != null && typeof validatableInput.value === "number"){
+        isValid = isValid && validatableInput.value >= validatableInput.min
+    }
+    if (validatableInput.max != null && typeof validatableInput.value === "number"){
+        isValid = isValid && validatableInput.value <= validatableInput.max
+    }
+    return isValid
+}
+
 
 //--NOTE; When we use Bind it needs to be on top of the class you want to use it in ---/
 
@@ -21,6 +59,41 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
 
 }
 
+//-- Project list Class
+class ProjectList{
+    //--This is a field ---//
+    templateElement: HTMLTemplateElement
+    hostElement: HTMLDivElement
+    element: HTMLElement
+
+    constructor(private type: "active" | "finished") {
+        this.templateElement = document.getElementById("project-list")! as HTMLTemplateElement //--This is type casing
+        this.hostElement = document.getElementById("app")! as HTMLDivElement
+        //--This imports our content--//
+        const importedNode = document.importNode(this.templateElement.content, true)
+
+        //-- Remember that this.element is the Section element
+        this.element = importedNode.firstElementChild as HTMLElement
+        this.element.id = `${this.type} -projects`
+        this.attach()
+        this.renderContent()
+    }
+
+    //---Method types here --- //
+
+    //--Here we want to fill the template that has nothing in the HTML ---//
+    private renderContent() {
+        //--We want to give the ul, h2 a Id so we go inside the -project-list template---//
+        const listId = `${this.type}-project-list`
+        this.element.querySelector("ul")!.id = listId
+        this.element.querySelector("h2")!.textContent = this.type.toLocaleUpperCase() + " MINI PROJECT"
+    }
+
+    //--This renders to the Dom ---//
+    private attach() {
+        this.hostElement.insertAdjacentElement("beforeend", this.element)
+    }
+}
 
 
 //--This is the ProjectInput Class--//
@@ -61,7 +134,29 @@ class  ProjectInput {
         const enteredDescription = this.descriptionInputElement.value;
         const enteredPeople = this.peopleInputElement.value;
 
-        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0 ) {
+        const titleValidatable: Validatable ={
+            value: enteredTitle,
+            required: true
+        }
+
+        const descriptionValidatable: Validatable ={
+            value: enteredDescription,
+          minLength: 5
+        }
+
+        const peopleValidatable: Validatable ={
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5
+        }
+
+//--We want to check if at least one of them is  false it will show the alert error
+        if (
+        !validate(titleValidatable) ||
+        !validate(descriptionValidatable) ||
+        !validate(peopleValidatable)
+        ) {
             alert("Invalid, please try again with a better input")
         } else{
             return [enteredTitle, enteredDescription, +enteredPeople]
@@ -102,3 +197,7 @@ class  ProjectInput {
 
 //-- This is how we will see the form on the website---//
 const prjInput = new ProjectInput()
+
+//--This is how we see out list selection ----//
+const activePrjList = new ProjectList("active")
+const finishPrjList = new ProjectList("finished")
